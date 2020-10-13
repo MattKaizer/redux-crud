10a6540fce061170e5fa397961fc21610cecd353
Matias Benjamín Moreno Juárez <matt@MBP-de-Matias.home>
2020-10-13 14:39:28 +0100

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
+        </nav>
+     );
+}
+ 
+export default Header;
