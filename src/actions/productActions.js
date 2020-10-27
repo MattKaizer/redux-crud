3f5742cf78ab9837f4034fd66c5fc5368a39d618
Matias Benjamín Moreno Juárez <matt@MBP-de-Matias.home>
2020-10-27 07:50:30 +0000

    adding new product

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,7 +1,33 @@
 import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
+import axiosClient from '../config/axios';
 
 export const addNewProductAction = (product) => {
-    return () => {
-        console.log(product);
+    return (dispatch) => {
+        dispatch(addProduct());
+
+        try {
+            // insert into API
+            axiosClient.post('/products', product);
+            // if its ok
+            dispatch(addProductSuccess(product));
+        } catch (error) {
+            console.log(error);
+            // if error
+            dispatch(addProductError(true));
+        }
     }
 }
+
+const addProduct = () => ({
+    type: ADD_PRODUCT
+})
+
+const addProductSuccess = product => ({
+    type: ADD_PRODUCT_SUCCESS,
+    payload: product
+})
+
+const addProductError = (state) => ({
+    type: ADD_PRODUCT_ERROR,
+    payload: state
+})
--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,4 +1,5 @@
 import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
+import axiosClient from '../config/axios';
 
 // every reducer have his own state
 const initialState = {
@@ -9,7 +10,23 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch(action.type) {
-
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                loading: true
+            }
+        case ADD_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                products: [...state.products, action.payload]
+            }
+        case ADD_PRODUCT_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
         default:
             return state;
     }
