4aba6145ac47bea07029f80ab053257662d95fd8
Matias Benjamín Moreno Juárez <matt@MBP-de-Matias.home>
2020-11-22 14:57:19 +0000

    adding other reducer

---
+++ src/reducers/alertReducer.js
@@ -0,0 +1,13 @@
+
+
+// EVERY REDUCER HAVE HIS OWN STAGE
+const initialState = {
+    alerta: null
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
